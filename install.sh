4bd9721194933f74e3638cee3b8c6ebaa6cbb0c1
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2022-04-08 01:47:21 +0200

    change order of commands

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# install docker and alternate configuration script
 DOCKER_REPO="https://raw.githubusercontent.com/kubicek/bullet_train_docker/main"
+
+# install and execute and alternate configuration script
+curl -o "bin/configure" "$DOCKER_REPO/bin/configure"
+bin/configure
+
+# install docker configuration
 curl -o "Dockerfile" "$DOCKER_REPO/Dockerfile"
 curl -o "docker-compose.yml" "$DOCKER_REPO/docker-compose.yml"
-curl -o "bin/configure" "$DOCKER_REPO/bin/configure"
 
-# modify files for more complex docker setup
-sed -i 's/-p 3000/-p 3000 -b 0.0.0.0/' Procfile.dev
-# not yet sure if needed
-#  insert_into_file "config/cable.yml", "  url: <%= ENV.fetch(\"REDIS_URL\") %>\n", :after => "adapter: redis\n"
+# puma should not listen to localhost only
+sed -i '' 's/-p 3000/-p 3000 -b 0.0.0.0/' Procfile.dev
 
+# commit changes
 git add .
 git commit -m "Added docker"
 
-bin/configure
-
+# build image
 docker compose build
+
+# replay instructions from configure
+echo "Next you can run `docker compose run web bin/setup` and then `docker compose up`."
